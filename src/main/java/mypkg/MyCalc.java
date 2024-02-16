7087b9be9114e94f27a4d540f136fa3785deaf9f
Rajeevbh <rajeevbh15290@gmail.com>
2024-02-17 03:16:23 +0530

    comment

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -24,6 +24,6 @@ public class MyCalc {
 		System.out.println("Sum is :"+calc.sum(20, 10));
 		System.out.println("Diff is :"+calc.diff(20, 10));
 		System.out.println("Multiplication is :"+calc.mul(20, 10));
-		System.out.println("Division is :"+calc.div(20, 10));
+		//System.out.println("Division is :"+calc.div(20, 10));
 	}
 }
